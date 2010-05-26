2a5f35578106710a4a6c33f21debc8f0608ec5a8
usm-data-analysis <usm.data.analysis@gmail.com>
2010-05-26 01:46:47 -0400

    Fix trailing whitespace of renecaceres/develop

--- analysis_lib/src/TIdentificator.cxx
+++ analysis_lib/src/TIdentificator.cxx
@@ -390,15 +390,11 @@ TString TIdentificator::GetCategorization(Int_t k)
             partId = "electron";
 
 
-        
         if (k > 0) {
+            if (Charge(k)==0 && Betta(k)>0.95 && ECStatus(k)>0)
+                partId = "photon";
 
-            if (Charge(k)==0 && 
-			Betta(k)>0.95 && 
-			ECStatus(k)>0) 
-			partId = "photon";
-
-	//positive particles	
+            //positive particles
             if (Charge(k) == 1 &&
                         Status(k) > 0 && Status(k) < 100 &&
                         StatDC(k) > 0 && DCStatus(k) > 0) {
