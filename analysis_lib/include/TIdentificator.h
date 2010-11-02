cfb14168936456b91bc0ec0cd79592d5657894af
smancill <smancill@alumnos.inf.utfsm.cl>
2010-11-02 01:50:05 -0300

    Add the missing 'inline' comments

--- analysis_lib/include/TIdentificator.h
+++ analysis_lib/include/TIdentificator.h
@@ -23,43 +23,43 @@ public:
     ~TIdentificator();
 
     // HEADER bank
-    Float_t NEvent();                           //inline
+    Float_t NEvent();                           // inline
 
     // EVNT bank
-    Double_t Betta(Int_t);                      //inline
-    Double_t Id(Int_t, Bool_t = 0);
-    Double_t Charge(Int_t);                     //inline
-    Double_t Px(Int_t, Bool_t = 0);
-    Double_t Py(Int_t, Bool_t = 0);
-    Double_t Pz(Int_t, Bool_t = 0);
-    Double_t X(Int_t);                          //inline
-    Double_t Y(Int_t);                          //inline
-    Double_t Z(Int_t);                          //inline
-    Int_t StatCC(Int_t);                        //inline
-    Int_t StatSC(Int_t);                        //inline
-    Int_t StatDC(Int_t);                        //inline
-    Int_t StatEC(Int_t);                        //inline
-    Double_t Status(Int_t);                     //inline
+    Double_t Betta(Int_t);                      // inline
+    Double_t Id(Int_t, Bool_t = 0);             // inline
+    Double_t Charge(Int_t);                     // inline
+    Double_t Px(Int_t, Bool_t = 0);             // inline
+    Double_t Py(Int_t, Bool_t = 0);             // inline
+    Double_t Pz(Int_t, Bool_t = 0);             // inline
+    Double_t X(Int_t);                          // inline
+    Double_t Y(Int_t);                          // inline
+    Double_t Z(Int_t);                          // inline
+    Int_t StatCC(Int_t);                        // inline
+    Int_t StatSC(Int_t);                        // inline
+    Int_t StatDC(Int_t);                        // inline
+    Int_t StatEC(Int_t);                        // inline
+    Double_t Status(Int_t);                     // inline
 
     // CCPB
-    Double_t Nphe(Int_t);
-    Double_t Chi2CC(Int_t);
-    Double_t CCStatus(Int_t);
+    Double_t Nphe(Int_t);                       // inline
+    Double_t Chi2CC(Int_t);                     // inline
+    Double_t CCStatus(Int_t);                   // inline
 
     // DCPB
-    Double_t DCStatus(Int_t);
+    Double_t DCStatus(Int_t);                   // inline
 
     // ECPB
-    Double_t Etot(Int_t k);
-    Double_t Ein(Int_t k);
-    Double_t Eout(Int_t k);
-    Double_t ECStatus(Int_t);
+    Double_t Etot(Int_t k);                     // inline
+    Double_t Ein(Int_t k);                      // inline
+    Double_t Eout(Int_t k);                     // inline
+    Double_t ECStatus(Int_t);                   // inline
 
     // SCPB
-    Double_t PathSC(Int_t k);
-    Double_t TimeSC(Int_t k);
-    Double_t EdepSC(Int_t k);
-    Double_t SCStatus(Int_t);
+    Double_t PathSC(Int_t k);                   // inline
+    Double_t TimeSC(Int_t k);                   // inline
+    Double_t EdepSC(Int_t k);                   // inline
+    Double_t SCStatus(Int_t);                   // inline
 
     // Derived observables
     Double_t Momentum(Int_t, Bool_t = 0);
