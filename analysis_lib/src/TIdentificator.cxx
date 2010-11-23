d59798157d5c183a70dc5d4bef37947945538c6d
royarzun <royarzun@gmail.com>
2010-11-23 15:28:54 -0300

    Fix problem with the Xb() function

--- analysis_lib/src/TIdentificator.cxx
+++ analysis_lib/src/TIdentificator.cxx
@@ -418,18 +418,18 @@ Double_t TIdentificator::Nu(Bool_t kind)
 
 Double_t TIdentificator::Xb(Bool_t kind){
     if(kind == 0)
-        return Q2(0)/2*Nu(0)*0.134;
+        return Q2(0)/(2*Nu(0)*massProton);
     else
-        return Q2(1)/2*Nu(1)*0.134;
+        return Q2(1)/(2*Nu(1)*massProton);
 }
 
 
 
 Double_t TIdentificator::Yb(Bool_t kind){
     if(kind == 0)
-        return Nu(0)/TIdentificator::kEbeam;
+        return Nu(0)/kEbeam;
     else
-        return Nu(1)/TIdentificator::kEbeam;
+        return Nu(1)/kEbeam;
 }
 
 
