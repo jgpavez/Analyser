434d994c1ec68f7fd4b4885cdb8872dcc6008acd
royarzun <royarzun@gmail.com>
2010-11-23 16:05:30 -0300

    Rename consts with the k Agreement

--- analysis_lib/include/massConst.h
+++ analysis_lib/include/massConst.h
@@ -3,18 +3,18 @@
 
 // Mass constants en GeV/c², the idea is keep on adding constants relevants in this kind of analysis
 
-    extern const Double_t massPi_plus = 0.13957018;
-    extern const Double_t massPi_min = 0.13957018;
-    extern const Double_t massPi_zero = 0.1349766;
-    extern const Double_t massKaon_plus = 0.493667;
-    extern const Double_t massKaon_min =0.493667;
-    extern const Double_t massKaon_zero = 0.497648;
-    extern const Double_t massMeu = 0.10565836;
-    extern const Double_t massAntiMeu = 0.10565836;
-    extern const Double_t massProton = 0.938272013;
-    extern const Double_t massAntiProton = 0.938;
-    extern const Double_t massNeutron = 0.939565560;
+    extern const Double_t kMassPi_plus = 0.13957018;
+    extern const Double_t kMassPi_min = 0.13957018;
+    extern const Double_t kMassPi_zero = 0.1349766;
+    extern const Double_t kMassKaon_plus = 0.493667;
+    extern const Double_t kMassKaon_min =0.493667;
+    extern const Double_t kMassKaon_zero = 0.497648;
+    extern const Double_t kMassMeu = 0.10565836;
+    extern const Double_t kMassAntiMeu = 0.10565836;
+    extern const Double_t kMassProton = 0.938272013;
+    extern const Double_t kMassAntiProton = 0.938;
+    extern const Double_t kMassNeutron = 0.939565560;
 
-//    extern const Double_t massDeuterium = 0;
+//    extern const Double_t kMassDeuterium = 0;
 
 #endif
