6615aece4f2b505d49c4133633d63b76a98f7196
smancill <smancill@alumnos.inf.utfsm.cl>
2010-12-14 09:56:31 -0300

    Improve content of 1_two_pion_C test directory

--- test/1_two_pion_C/two_pion_C_data.cxx
+++ test/1_two_pion_C/two_pion_C_data.cxx
@@ -1,21 +1,18 @@
-#include "Riostream.h"
-#include "TH1F.h"
-#include "TTree.h"
-#include "TCanvas.h"
-#include "TFile.h"
-#include "TApplication.h"
+#if !defined(__CINT__)
 #include "TROOT.h"
-#include <math.h>
+#include "Riostream.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include <cmath>
+#endif
 
-extern void InitGui();
-VoidFuncPtr_t initfuncs[] = {InitGui, 0};
 
-TROOT my_app("myapp", "myapp", initfuncs);
-
-int main(int argc, char **argv)
+void two_pion_C_data(void)
 {
     gROOT->Reset();
-    TApplication *rootapp = new TApplication("Tarea1", &argc, argv);
 
     // Get the root file
     TFile *f = new TFile("two_pion_C_data.root");
@@ -38,16 +35,18 @@ int main(int argc, char **argv)
     TH1F *h1 = new TH1F("Q12", "Q12", 300, 0, 3);
     TH1F *h2 = new TH1F("Q12_mix", "Q12_mix", 300, 0, 3);
 
+    // Fill the histograms
     Float_t q, cx, cy, cz;
     Float_t temp1x, temp1y, temp1z, tempx, tempy, tempz;
 
-    // Fill the histograms
+    std::cout << "Processing data..." << std::endl;
+
     t1->GetEntry(0);
 
     cx = px[0] - px[1];
     cy = py[0] - py[1];
     cz = pz[0] - pz[1];
-    q = sqrt(cx * cx  +  cy * cy  +  cz * cz);
+    q = std::sqrt(cx * cx  +  cy * cy  +  cz * cz);
     
     tempx = px[0];
     tempy = py[0];
@@ -57,21 +56,21 @@ int main(int argc, char **argv)
     temp1z = pz[1];
 
     h1->Fill(q);  
-    cout << "Procesando datos...\n";
+
     for (Int_t i = 1; i < nentries; i++) {
         t1->GetEntry(i);
 
         cx = px[0] - px[1];
         cy = py[0] - py[1];
         cz = pz[0] - pz[1];
-        q = sqrt(cx * cx  +  cy * cy  +  cz * cz);
+        q = std::sqrt(cx * cx  +  cy * cy  +  cz * cz);
         
         h1->Fill(q);  
 
         cx = tempx - px[1];
         cy = tempy - py[1];
         cz = tempz - pz[1];
-        q = sqrt(cx * cx  +  cy * cy  +  cz * cz);
+        q = std::sqrt(cx * cx  +  cy * cy  +  cz * cz);
         h2->Fill(q);  
 
         tempx = px[0];
@@ -82,13 +81,13 @@ int main(int argc, char **argv)
     cx = tempx - temp1x;
     cy = tempy - temp1y;
     cz = tempz - temp1z;
-    q = sqrt(cx * cx  +  cy * cy  +  cz * cz);
+    q = std::sqrt(cx * cx  +  cy * cy  +  cz * cz);
     h2->Fill(q); 
      
 
+    std::cout << "Generating histograms..." << std::endl;
     TCanvas *c = new TCanvas("c", "Tarea 1", 1280, 800);
-    cout << "Generando histogramas...\n";
-    c->Divide(2, 2);    
+    c->Divide(2,2);
 
     // Show Q12 histogram
     c->cd(1);
@@ -105,14 +104,28 @@ int main(int argc, char **argv)
     h3->SetTitle("R");
     h3->SetName("R");
     h3->Sumw2();
-    h3->Divide(h1, h2);
+    h3->Divide(h1,h2);
     h3->SetLineColor(kBlue);
 
     // Show R histogram
     c->cd(3);
     h3->Draw();
+}
 
-//    rootapp->Run(kTRUE);
+
+#if !defined(__CINT__)
+extern void InitGui();
+VoidFuncPtr_t initfuncs[] = {InitGui, 0};
+
+TROOT my_app("myapp", "myapp", initfuncs);
+
+
+int main(int argc, char **argv)
+{
+    gROOT->SetStyle("Plain");
+    TApplication *rootapp = new TApplication("Show histogram", &argc, argv);
+    two_pion_C_data();
     rootapp->Run();
     return 0;
 }
+#endif
